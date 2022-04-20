2f50a39cab05bedfd7dc0a6b5a472f50001edf5d
codejihyekim <wlgp03090@gmail.com>
2022-04-20 12:42:02 +0900

    2022-04-20 12:41 import 구조로 변경

--- app.js
+++ app.js
@@ -1,39 +1,46 @@
-require('dotenv').config();
-const cors = require('cors')
-const express = require('express');
-const app = express();
-const { port, MONGO_URI } = process.env;
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
-app.use(cors());
-const APP = './app/routes'
-//const nodes = ['admin','basic','board','game','user']
-const nodes = ['basic','board','user','todo']
-for(const leaf of nodes){
-  require(`${APP}/${leaf}.route`)({url:`/api/${leaf}`,app})
-}
-const corsOptions = {
-  origin: 'http://localhost:3000', 
-  optionsSuccessStatus: 200 
-}
-const db = require('./app/models/index')
-db.mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+import dotenv from 'dotenv'
+import cors from 'cors'
+import express from 'express'
+import db from './app/models/index.js'
+
+async function startServer(){
+  dotenv.config()
+  const app = express()
+  const mongoUri = process.env.MONGO_URI
+  const port = process.env.PORT
+  app.use(express.static('public'));
+  app.use(express.urlencoded({ extended: true}));
+  app.use(express.json());
+  app.use(cors());
+
+  const corsOptions = {
+    origin: 'http://localhost:3000', 
+    optionsSuccessStatus: 200 
+  }
+
+  db.mongoose
+  .connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(()=>{
     console.log(' ### 몽고DB 연결 성공 ###')
   })
   .catch(err => { console.log(' 몽고DB와 연결 실패 ', err)
         process.exit();
-});
-app.listen(port, () => {
-  console.log('***************** ***************** *****************')
-  console.log('***************** 서버가 정상적으로 실행되고 있습니다 *****************')
-  console.log('***************** ***************** *****************')
-})
-app.get('/', (req, res) => {
-  res.json({"현재 시간 : ":new Date().toLocaleString()})
-})
-app.get('/api/now', cors(corsOptions), (req,res) => {
-  res.json({"now": new Date().toLocaleString()})
-})
+  });
+  app.listen(port, () => {
+    console.log('***************** ***************** *****************')
+    console.log('***************** 서버가 정상적으로 실행되고 있습니다 *****************')
+    console.log('***************** ***************** *****************')
+  })
+  app.get('/', (req, res) => {
+    res.json({"현재 시간 : ":new Date().toLocaleString()})
+  })
+  app.get('/api/now', cors(corsOptions), (req,res) => {
+    res.json({"now": new Date().toLocaleString()})
+  })
+}
+startServer()
+
+
+
+
+
